3f707ee09e322bc16947b01f03a13dc6b8ab9569
masahiro fukatsu <masa.1108@icloud.com>
2021-12-11 22:44:32 +0900

    Firebaseの初期設定完了

---
+++ utils/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+export const firebaseApp = initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_FIRESTORE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIRESTORE_APP_ID,
+  measurementId: "${config.measurementId}"
+});
+
+export const db = getFirestore();
